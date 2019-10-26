60354b9683906e9fb95134bad6240d39878edcfd
Naga <nagabrahmam9@yahoo.com>
2019-10-26 09:58:24 -0400

    Changed Parent theme

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 function enqueue_styles() {
-	wp_enqueue_style( 'parent_css', get_template_directory_uri() . '/style.css', array(), filemtime(get_template_directory_uri() . '/style.css'), false);
-	wp_enqueue_style('femme_css', get_stylesheet_directory_uri().'/style.css', array(), filemtime(get_stylesheet_directory() . '/style.css'), false);
+	wp_enqueue_style( 'parent_css', get_template_directory_uri() . '/style.css');
+	wp_enqueue_style('femme_css', get_stylesheet_directory_uri().'/style.css');
 }
 add_action( 'wp_enqueue_scripts', 'enqueue_styles' );
